a05f4b671a2334316cf4da7acf12889e1c6592a4
peter_alfie <alfie1104@gmail.com>
2023-10-18 08:33:14 +0900

    feat(getapp): getApp is done

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,6 +5,7 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant?: string;
+  full?: boolean;
 };
 
 const Button = ({
@@ -12,14 +13,19 @@ const Button = ({
   title,
   icon,
   variant = "btn_dark_green",
+  full,
 }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
